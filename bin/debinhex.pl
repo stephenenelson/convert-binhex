3caadd413c177681f3fa9c6a1895a0044f4d00f9
Stephen Nelson <stephenenelson@mac.com>
2013-09-06 20:32:38 -0700

    Fixed error in pod of debinhex and added pod test

--- bin/debinhex.pl
+++ bin/debinhex.pl
@@ -1,5 +1,6 @@
 #!/usr/bin/perl -w
 
+=encoding UTF-8
 
 =head1 NAME
 
@@ -34,7 +35,7 @@ Largely untested.
 Paul J. Schinder (NASA/GSFC) mostly, though Eryq can't seem to keep
 his grubby paws off anything...
 
-Sören M. Andersen (somian), made it actually work under Perl 5.8.7 on MSWin32.
+Sšren M. Andersen (somian), made it actually work under Perl 5.8.7 on MSWin32.
 
 =cut
 
